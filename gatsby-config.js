2572653cf2e681e7b912219a91beb5cf12425234
Cormac McGloin <v-cormac.mcgloin@zoopla.co.uk>
2020-04-23 13:55:08 +0100

    Added Google Analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,21 @@ module.exports = {
     siteUrl: `https://www.cormacmcgloin.com`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-48570651-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 1,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify`,
     `gatsby-plugin-transition-link`,
